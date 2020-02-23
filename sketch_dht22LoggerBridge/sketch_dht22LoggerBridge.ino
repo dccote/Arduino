9edcc1374bbc81d71ebda8c12e226c3b51831363
Daniel Côté <dccote@novajo.ca>
2020-02-23 14:20:31 -0500

    Renamed files for clarity, rewrote logger bridge

---
+++ sketch_dht22LoggerBridge/sketch_dht22LoggerBridge.ino
@@ -0,0 +1,22 @@
+#define computer Serial
+#define logger Serial1
+
+
+void setup() {
+  computer.begin(9600);
+  logger.begin(9600);
+}
+
+void loop() {
+  int c;
+  if (logger.available()) {
+    while ( (c = logger.read()) != -1 ) {
+       computer.write(c);
+    } 
+  } else if ( computer.available()) {  
+    while ( (c = computer.read()) != -1 ) {
+       logger.write(c);
+    } 
+  }
+  delay(100);
+}
