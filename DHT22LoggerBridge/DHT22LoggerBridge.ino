c520b19b199aad059c3660131ae35e74fec807a5
Daniel Côté <dccote@novajo.ca>
2020-02-23 14:57:02 -0500

    Update DHT22LoggerBridge.ino
    Commented code, cleaned up.

--- DHT22LoggerBridge/DHT22LoggerBridge.ino
+++ DHT22LoggerBridge/DHT22LoggerBridge.ino
@@ -1,3 +1,15 @@
+/*
+ * This communicates with another Arduino device by serial connection
+ * it must be connected on Serial1, which on the Mega is on pins
+ * 19(RX) and 18(TX) and must operate at 9600 bps.
+ * 
+ * It will simply forward (or bridge) the characters
+ * received on its main Serial port to the Serial1 port.
+ * 
+ * This is useful if another arduino is running on a USB powered battery
+ * and we want to extract information from it without disconnecting it.
+ */
+
 #define computer Serial
 #define logger Serial1
 
