75b73f989670c263072ff1209308313692d27ed2
Daniel Côté <dccote@novajo.ca>
2020-02-23 22:57:54 -0500

    More libraries for Otto the Robot.
    Otto the robot is a good example of Nano controlling motors and a ranging sensor.
    Parts of the code were changed to accomodate Otto9, a newer version that did not fit with the available examples code.

---
+++ Otto/Otto_servoBase/Otto_servoBase.ino
@@ -0,0 +1,29 @@
+/*
+ Controlling a servo position using a potentiometer (variable resistor)
+ by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
+
+ modified on 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Knob
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+
+void setup() {
+  myservo.attach(2);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+//  myservo.write(val);                  // sets the servo position according to the scaled value
+//  if (val == 180) {
+//    val = 0;
+//  } else {
+//    val = 180;
+//  }
+//  delay(1000);                           // waits for the servo to get there
+}
