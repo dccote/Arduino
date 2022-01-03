d5a3305baa3b0c3591bd5fb9344f4026f368f84f
Daniel <dccote@cervo.ulaval.ca>
2022-01-02 20:30:32 -0500

    Update SoundMonitor.ino
    Correction, cleanup

--- SoundMonitor/SoundMonitor.ino
+++ SoundMonitor/SoundMonitor.ino
@@ -1,12 +1,9 @@
 /*
-  This example reads audio data from the on-board PDM microphones, and prints
-  out the samples to the Serial console. The Serial Plotter built into the
-  Arduino IDE can be used to plot the audio data (Tools -> Serial Plotter)
+  This is a simple sound monitor I made by reusing a lot of code from examples.
 
-  Circuit:
-  - Arduino Nano 33 BLE Sense board
-
-  This example code is in the public domain.
+  With a Nano BLE Sense 33, we use the microphone, compute the spectrum and
+  show it on an NeoPixel 16-LED circular element.
+  
 */
 
 
@@ -17,7 +14,7 @@
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
-#define PIXEL_PIN    12  // Digital IO pin connected to the NeoPixels.
+#define PIXEL_PIN   12  // Digital IO pin connected to the NeoPixels.
 #define PIXEL_COUNT 16  // Number of NeoPixels
 
 // Declare our NeoPixel strip object:
@@ -31,12 +28,7 @@ Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, NEO_GRBW + NEO_KHZ800);
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
-
-
-
 /* Microphone variables */
-
-
 #define N_SAMPLES 256
 short sampleBuffer[N_SAMPLES]; // buffer to read samples into, each sample is 16-bits
 volatile int samplesRead;      // number of samples read
@@ -50,24 +42,23 @@ Input vectors receive computed results from FFT
 double vReal[N_SAMPLES];
 double vImag[N_SAMPLES];
 
+void binSoundHistogram(int8_t binSize = (N_SAMPLES/2/2)/PIXEL_COUNT);
+void showSoundLevels(double maxValue = 5000);
 
 void setup() {
+  /* Setup serial port */
   Serial.begin(9600);
   while (!Serial);
 
+  /* Setup neopixel strip */
   strip.begin(); // Initialize NeoPixel strip object (REQUIRED)
   strip.show();  // Initialize all pixels to 'off'
 
-  // configure the data receive callback
-  PDM.onReceive(onPDMdata);
+  /* Setup microphone */
+  PDM.onReceive(onPDMdata); // callback
+  PDM.setGain(20);          
 
-  // optionally set the gain, defaults to 20
-  PDM.setGain(10);
-
-  // initialize PDM with:
-  // - one channel (mono mode)
-  // - a 16 kHz sample rate
-  if (!PDM.begin(1, 16000)) {
+  if (!PDM.begin(1, 16000)) { // mono at 16 kHz
     Serial.println("Failed to start PDM!");
     while (1);
   }
@@ -75,9 +66,11 @@ void setup() {
 }
 
 void loop() {
-  // wait for samples to be read
+  // When we have samples, we manipulate the spectrum
   if (samplesRead ) {
-    computeSpectrum();
+    computeSoundSpectrum();
+    binSoundHistogram();
+    showSoundLevels();
   }
 
 //  soundAlarm();
@@ -92,7 +85,6 @@ void soundAlarm() {
 
 }
 
-
 void onPDMdata() {
   // query the number of bytes available
   int bytesAvailable = PDM.available();
@@ -104,8 +96,7 @@ void onPDMdata() {
   samplesRead = bytesAvailable / 2;
 }
 
-
-void computeSpectrum() {
+void computeSoundSpectrum() {
   
   for (uint16_t i = 0; i < N_SAMPLES; i++) {
     vReal[i] = double(sampleBuffer[i]);
@@ -116,13 +107,9 @@ void computeSpectrum() {
   FFT.ComplexToMagnitude(vReal, vImag, N_SAMPLES); /* Compute magnitudes */
   vReal[0] = 0.;  // Remove DC component, always very large
 
-  binSoundHistogram();
-  
-  showSoundLevels();
 }
 
-void showSoundLevels() {
-  double maxValue = 5000;
+void showSoundLevels(double maxValue) {
   uint8_t intensity[16];
   for (uint16_t i = 0; i < 16; i++) {
      intensity[i] = uint8_t(vReal[i]/maxValue*255.0);
@@ -139,9 +126,9 @@ void showSoundLevels() {
 
 }
 
-void binSoundHistogram() {
+void binSoundHistogram(int8_t binSize) {
   double maxValue = 0;
-  int8_t binSize = (N_SAMPLES/2/2)/16;
+
   for (uint16_t i = 0; i < 16; i++) {
     double sum = 0;
     for (uint16_t j = 0; j < binSize; j++) {
@@ -150,86 +137,6 @@ void binSoundHistogram() {
     vReal[i] = sum;
     if (sum > maxValue) {
       maxValue = sum;
-    }
-  }
-}
-
-void normaliseSound(double factor) {
-  
-}
-
-// Fill strip pixels one after another with a color. Strip is NOT cleared
-// first; anything there will be covered pixel by pixel. Pass in color
-// (as a single 'packed' 32-bit value, which you can get by calling
-// strip.Color(red, green, blue) as shown in the loop() function above),
-// and a delay time (in milliseconds) between pixels.
-void colorWipe(uint32_t color, int wait) {
-  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-    strip.show();                          //  Update strip to match
-    delay(wait);                           //  Pause for a moment
-  }
-}
-
-// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
-// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
-// between frames.
-void theaterChase(uint32_t color, int wait) {
-  for(int a=0; a<10; a++) {  // Repeat 10 times...
-    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
-      // 'c' counts up from 'b' to end of strip in steps of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
-        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
-      }
-      strip.show(); // Update strip with new contents
-      delay(wait);  // Pause for a moment
-    }
-  }
-}
-
-// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void rainbow(int wait) {
-  // Hue of first pixel runs 3 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 3*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 3*65536/256 = 768 passes through this outer loop:
-  for(long firstPixelHue = 0; firstPixelHue < 3*65536; firstPixelHue += 256) {
-    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-      // Offset pixel hue by an amount to make one full revolution of the
-      // color wheel (range of 65536) along the length of the strip
-      // (strip.numPixels() steps):
-      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
-      // optionally add saturation and value (brightness) (each 0 to 255).
-      // Here we're using just the single-argument hue variant. The result
-      // is passed through strip.gamma32() to provide 'truer' colors
-      // before assigning to each pixel:
-      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
-    }
-    strip.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
-  }
-}
-
-// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
-void theaterChaseRainbow(int wait) {
-  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
-  for(int a=0; a<30; a++) {  // Repeat 30 times...
-    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
-      // 'c' counts up from 'b' to end of strip in increments of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
-        // hue of pixel 'c' is offset by an amount to make one full
-        // revolution of the color wheel (range 65536) along the length
-        // of the strip (strip.numPixels() steps):
-        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
-        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
-        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
-      }
-      strip.show();                // Update strip with new contents
-      delay(wait);                 // Pause for a moment
-      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
     }
   }
 }
